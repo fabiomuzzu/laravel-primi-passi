50df9bbc44e6d0acf21bf8f77d6731055f4c368e
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-02-09 15:42:59 +0100

    added navbar in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,12 +8,42 @@
     <title>Laravel Hello World</title>
 </head>
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h1 class="text-center my-3">{{$welcome}}</h1>
+    
+    {{-- HEADER --}}
+    <header>
+        <nav class="navbar navbar-expand" style="background-color: #e3f2fd;">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Boolean Class #113</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Documents</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">About Us</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    {{-- MAIN --}}
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="text-center my-3">{{$welcome}}</h1>
+                </div>
             </div>
         </div>
-    </div>
+    </main>
 </body>
 </html>
